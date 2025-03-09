a343c66c02f7a75311b4fc6c33070789f2bfe753
Kyle Robertson <kyle59287@tutanota.com>
2025-03-09 16:17:46 -0700

    Fixed is_adjacent

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -36,7 +36,7 @@ bool edit_distance_within(const std::string &str1, const std::string &str2, int 
 }
 
 bool is_adjacent(const string &word1, const string &word2) {
-    return !edit_distance_within(word1, word2, 0) && edit_distance_within(word1, word2, 1);
+    return edit_distance_within(word1, word2, 1);
 }
 
 vector<string> generate_word_ladder(const string &begin_word, const string &end_word, const set<string> &word_list) {
